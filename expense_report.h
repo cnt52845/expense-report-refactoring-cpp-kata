b4e0dbccdb5fd3e946f6611ee2bfa810c9b705c2
Replit user <>
2023-12-06 09:29:31 +0000

    Moved printing report into ExpenseReporter class

--- expense_report.h
+++ expense_report.h
@@ -77,50 +77,76 @@ class ExpenseReportNamer : public ExpenseNamer {
 class ExpenseReport {
 private:
     std::vector<std::shared_ptr<Expense>> expenses;
-    std::shared_ptr<ExpenseNamer>         namer = std::make_shared<ExpenseReportNamer>();
+    double                                total      = 0;
+    double                                meal_total = 0;
+
+    void add_to_totals(const std::shared_ptr<Expense>& expense)
+    {
+        double subtotal = expense->get_amount() + expense->get_surcharge();
+        total += subtotal;
+        if (expense->is_meal()) {
+            meal_total += subtotal;
+        }
+    }
 
 public:
     void add_expense(const std::shared_ptr<Expense>& expense) { expenses.push_back(expense); }
 
-    void print_report(ReportPrinter& printer)
+    void total_up_expenses()
     {
-        printer.print("Expense Report");
-        printer.print("--------------");
-
-        double total      = 0;
-        double meal_total = 0;
         for (const std::shared_ptr<Expense>& expense : expenses) {
-            double      surcharge                 = expense->get_surcharge();
-            std::string name                      = namer->get_name(expense);
+            add_to_totals(expense);
+        }
+    }
+
+    const std::vector<std::shared_ptr<Expense>>& get_expenses() { return expenses; }
+
+    double get_expenses_total() { return total; }
+
+    double get_meal_expenses_total() { return meal_total; }
+};
+
+class ExpenseReporter {
+private:
+    const std::shared_ptr<ExpenseReport> report;
+    std::shared_ptr<ExpenseNamer>        namer = std::make_shared<ExpenseReportNamer>();
+    ReportPrinter*                       printer;
+
+    void print_expenses_and_totals()
+    {
+        printer->print("Expense Report");
+        printer->print("--------------");
+
+        for (const std::shared_ptr<Expense>& expense : report->get_expenses()) {
             std::string meal_over_expenses_marker = "";
 
-            total += expense->get_amount() + surcharge;
             if (expense->is_meal()) {
-                meal_total += expense->get_amount() + surcharge;
                 if (expense->is_overage()) {
                     meal_over_expenses_marker = "X";
                 }
             }
 
             std::ostringstream oss;
-            oss << std::fixed << std::setprecision(2) << name << "\t"
+            oss << std::fixed << std::setprecision(2) << namer->get_name(expense) << "\t"
                 << expense->get_amount() / 100.0 << "\t" << meal_over_expenses_marker;
-            printer.print(oss.str());
+            printer->print(oss.str());
         }
 
-        printer.print("--------------");
+        printer->print("--------------");
         std::ostringstream oss;
-        oss << "Meal Total: " << std::fixed << std::setprecision(2) << meal_total / 100.0 << "\n";
-        oss << "Total: " << std::fixed << std::setprecision(2) << total / 100.0;
-        printer.print(oss.str());
+        oss << "Meal Total: " << std::fixed << std::setprecision(2)
+            << report->get_meal_expenses_total() / 100.0 << "\n";
+        oss << "Total: " << std::fixed << std::setprecision(2)
+            << report->get_expenses_total() / 100.0;
+        printer->print(oss.str());
     }
-};
-
-class ExpenseReporter {
-private:
-    const std::shared_ptr<ExpenseReport> report;
 
 public:
     ExpenseReporter(std::shared_ptr<ExpenseReport>& report) : report(report) {}
-    void print_report(ReportPrinter& printer) { report->print_report(printer); }
+    void print_report(ReportPrinter& printer)
+    {
+        this->printer = &printer;
+        report->total_up_expenses();
+        print_expenses_and_totals();
+    }
 };
